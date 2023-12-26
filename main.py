6f336f1497a6284f928eb4ddf77ee09b4e96a409
VitaliyKozymko <154830692+VitaliyKozymko@users.noreply.github.com>
2023-12-26 17:53:14 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# Simple example of using your project
+
+#Input login
+login = input("Enter login: ")
+
+#Check login ("my own Login: "")
+if login == "Your login: ":
+    print("Login is Correct! ")
+else:
+    print("Login is Incorrect! ")
+
+#Input password
+password = input("Enter Password: ")
+
+#Check password (my own password )
+if password == "Your Password":
+    print("Password is Correct!")
+else:
+    print("Password is Incorrect")
